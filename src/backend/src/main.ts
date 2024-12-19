8d126bdda42b1fabdc8a38c0b8f0bf95652ef88e
nusretkilic <186486003+nusretkilic@users.noreply.github.com>
2024-12-19 20:59:37 +0100

    Set up CORS configuration for the application

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  console.log("CORS:", process.env.CORS_ORIGIN);
+
+  // Enable Cors
+  app.enableCors( {
+    origin: process.env.CORS_ORIGIN,
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+    allowedHeaders: "Content-Type, Accept",
+  });
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
